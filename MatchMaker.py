c6635e11de2bcdb2395dad599c5277655834ad3d
Chris Glessner <glessner.chris@gmail.com>
2019-10-26 21:57:09 -0400

    Get routes working

--- MatchMaker.py
+++ MatchMaker.py
@@ -1,18 +1,25 @@
+import os
 import pandas as pd
 from sqlalchemy.ext.automap import automap_base
 #from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, session
 
-from config import pguser, pgpassword, pghost, pgport, pgdatabase
+from config import pguser, pgpassword, pghost, pgport, pgdatabase, secret_key
 
 engine = create_engine(
     f"postgresql+psycopg2://{pguser}:{pgpassword}" + \
     f"@{pghost}:{pgport}/{pgdatabase}"
 )
 
+#############################
+#       SET UP DATABASE     #
+#############################
 df = pd.read_sql('matchdata',
                  engine)
+
+users = pd.read_sql('Users',
+                    engine)
 
 Base = automap_base()
 
@@ -20,12 +27,25 @@ Base = automap_base()
 Base.prepare(engine,
              reflect=True)
 
+
+#############################
+#     HELPFUL FUNCTIONS     #
+#############################
+
+def emailExists(email):
+    u = users.loc[users['email'] == email]
+
+    return bool(u.shape[0])
+
+def nextIID():
+    iids = users['iid'].to_list()
+    iids = [int(iid) for iid in iids]
+    return str(max(iids) + 1)
+
+
 app = Flask(__name__)
 
-#print users
-users = pd.read_sql('Users',
-                    engine)
-print(users)
+app.secret_key = secret_key
 
 # Homepage
 @app.route("/")
@@ -35,38 +55,31 @@ def homepage():
     # Format the data to send as json
     return render_template("index.html")
 
-# Login
-@app.route("/login")
+# Query the database and send the jsonified results
+@app.route("/login", methods=["GET", "POST"])
 def login():
     """Return the login page."""
-
-
-    # Format the data to send as json
-    return render_template("login.html")
-
-# Query the database and send the jsonified results
-"""
-@app.route("/login_", methods=["GET", "POST"])
-def login_():
     if request.method == "POST":
-        email = request.form["userEmail"]
+        email    = request.form["userEmail"]
         password = request.form["userPassword"]
+        session['email'] = email
 
-        db.session.add(pet)
-        db.session.commit()
-        return redirect("/", code=302)
+        if emailExists(email):
+            iid = users.loc[users['email'] == f'{email}']
+            return render_template("matches.html",
+                                   iid=iid)
+        else:
+            return render_template("register.html")
 
-    if userindata:
-        return render_template("matches.html")
-    else:
-        return render_template("register.html")
-    """
+    return render_template("login.html")
 
 # Register
 # TODO: Set this up so that you can either pass the details or start new
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
     """Return the registration page."""
+    if request.method == "POST":
+        return render_template('register.html')
 
     # Format the data to send as json
     return render_template("register.html")
